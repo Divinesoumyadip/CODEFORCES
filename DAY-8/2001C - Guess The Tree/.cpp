7f7e76eceebcebf2ec4870e6d89518e1dd7ffdc6
Soumyadip  Das Mahapatra <172062256+Divinesoumyadip@users.noreply.github.com>
2024-09-18 12:00:39 +0530

    Create .cpp

---
+++ DAY-8/2001C - Guess The Tree/.cpp
@@ -0,0 +1,2 @@
+#import<iostream>
+using namespace std;int e[2001],n,l,x,y;main(){for(cin>>n;cin>>n;cout<<endl){for(l=1;n>1;e[l++]=x,e[l++]=x=n--)for(y=1;x!=y;cin>>y)printf("? %d %d",x=y,n),cout<<endl;for(cout<<"!";--l;)printf(" %d",e[l]);}}
