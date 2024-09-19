814ac66889339dddd9fc8a0d959c8c5e9f763e0e
Soumyadip  Das Mahapatra <172062256+Divinesoumyadip@users.noreply.github.com>
2024-09-20 01:38:34 +0530

    Create .cpp

---
+++ Day-10/2003E1 - Turtle and Inversions (Easy Version)/.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int m,n;
+        cin >> n >> m;
+        pair<int,int> pr[m];
+        for(int i=0;i<m;i++)
+        {
+            cin >> pr[i].first >> pr[i].second;
+        }
+        long long int sum=1LL*n*(n-1)/2;
+        for(int i=0,j=m-1;i<=j;i++,j--)
+        {
+            sum=sum-(pr[j].second-pr[i].first);
+        }
+ 
+        cout << sum << endl;
+    }
+ 
+    return 0;
+}
